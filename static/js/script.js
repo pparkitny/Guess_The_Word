487f71aff5826de60ce9f0dbaef4eb913b7786d6
Patryk <pat.parkitny@gmail.com>
2022-05-21 11:27:37 +0200

    Add jumping to previous input while type backspace key

--- static/js/script.js
+++ static/js/script.js
@@ -8,20 +8,55 @@ a.onkeyup = function() {
     if (this.value.length === parseInt(this.attributes["maxlength"].value)) {
         b.focus();
     }
+    this.addEventListener('keydown', function(event) {
+        const key = event.key; 
+        if (key === "Backspace") {
+            if (this.value.length < parseInt(this.attributes["maxlength"].value)) {a.focus()};
+        }
+    });
 }
 
 b.onkeyup = function() {
     if (this.value.length === parseInt(this.attributes["maxlength"].value)) {
         c.focus();
     }
+    this.addEventListener('keydown', function(event) {
+        const key = event.key;
+        if (key === "Backspace") {
+            if (this.value.length < parseInt(this.attributes["maxlength"].value)) {a.focus()};
+        }
+    });
 }
+
 c.onkeyup = function() {
     if (this.value.length === parseInt(this.attributes["maxlength"].value)) {
         d.focus();
     }
+    this.addEventListener('keydown', function(event) {
+        const key = event.key;
+        if (key === "Backspace") {
+            if (this.value.length < parseInt(this.attributes["maxlength"].value)) {b.focus()};  
+        }
+    });
 }
+
 d.onkeyup = function() {
     if (this.value.length === parseInt(this.attributes["maxlength"].value)) {
         e.focus();
     }
+    this.addEventListener('keydown', function(event) {
+        const key = event.key;
+        if (key === "Backspace") {
+            if (this.value.length < parseInt(this.attributes["maxlength"].value)) {c.focus()};  
+        }
+    });
+}
+
+e.onkeyup = function() {
+    this.addEventListener('keydown', function(event) {
+        const key = event.key;
+        if (key === "Backspace") {
+            if (this.value.length < parseInt(this.attributes["maxlength"].value)) {d.focus()};
+        }
+    });
 }
