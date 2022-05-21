9a93080bbdee9e6e185e24d86b2ff0d26fad54ca
Patryk <pat.parkitny@gmail.com>
2022-05-21 13:22:59 +0200

    Add comments to js file

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,22 @@
-var elements = document.getElementsByClassName('box');
-var requiredElement = elements[0];
+var elements = document.getElementsByClassName('box'); // get all elements with class 'box'
+var requiredElement = elements[0]; // get first element with class 'box'
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function(event) { // listening for typing on keyboard
     const key = event.key;
-    var letters = /^[A-Za-z]+$/;
-    if (key === "Backspace") {
+    var letters = /^[A-Za-z]+$/; // only letters
+    if (key === "Backspace") { // check if user typre 'Backspace'
         var delElement = Array.from(
-            document.getElementsByClassName('boxDone')
+            document.getElementsByClassName('boxDone') // get last element with class 'boxDone'
           ).pop();
-        delElement.innerHTML = "";
-        delElement.className = "box";
-        requiredElement = elements[0]
+        delElement.innerHTML = ""; // change value of div to empty
+        delElement.className = "box"; // change name of class to 'box'
+        requiredElement = elements[0] // again get first element with 'box' class
     }
-    else if(String.fromCharCode(event.keyCode).match(letters)) {
-        if(document.getElementsByClassName('boxDone').length < 5){
-            requiredElement.innerHTML += key;
-            requiredElement.className = "boxDone";
-            requiredElement = elements[0]
+    else if(String.fromCharCode(event.keyCode).match(letters)) { // check if user type letter
+        if(document.getElementsByClassName('boxDone').length < 5){ // check if there is only 5 divs with class "boxDone"
+            requiredElement.innerHTML += key; // change value of div to key that user typed
+            requiredElement.className = "boxDone"; // change name of class to "boxDone"
+            requiredElement = elements[0] // again get first element with class name "box"
      }
     }
 });
