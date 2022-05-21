a5ff317109cea47d61eeda55044b3167e8a843b6
Patryk <pat.parkitny@gmail.com>
2022-05-21 12:40:13 +0200

    Change conception of app working - change inputs to divs and script from taking input values to changing innerHTML in divs and their classnames on each key type

--- static/js/script.js
+++ static/js/script.js
@@ -1,52 +1,19 @@
-var a = document.getElementById("a"),
-    b = document.getElementById("b"),
-    c = document.getElementById("c"),
-    d = document.getElementById("d"),
-    e = document.getElementById("e");
+var elements = document.getElementsByClassName('box');
+var requiredElement = elements[0];
 
-a.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function(event) {
     const key = event.key;
     if (key === "Backspace") {
-        if (this.value.length < parseInt(this.attributes["maxlength"].value)) {a.focus()};
+        var delElement = Array.from(
+            document.getElementsByClassName('boxDone')
+          ).pop();
+        delElement.innerHTML = "";
+        delElement.className = "box";
+        requiredElement = elements[0]
     }
-    else if (this.value.length === parseInt(this.attributes["maxlength"].value)) {
-        b.focus();
-    }
-});
-
-b.addEventListener('keydown', function(event) {
-    const key = event.key;
-    if (key === "Backspace") {
-        if (this.value.length < parseInt(this.attributes["maxlength"].value)) {a.focus()};
-    }
-    else if (this.value.length === parseInt(this.attributes["maxlength"].value)) {
-        c.focus();
-    }
-});
-
-c.addEventListener('keydown', function(event) {
-    const key = event.key;
-    if (key === "Backspace") {
-        if (this.value.length < parseInt(this.attributes["maxlength"].value)) {b.focus()};
-    }
-    else if (this.value.length === parseInt(this.attributes["maxlength"].value)) {
-        d.focus();
-    }
-});
-
-d.addEventListener('keydown', function(event) {
-    const key = event.key;
-    if (key === "Backspace") {
-        if (this.value.length < parseInt(this.attributes["maxlength"].value)) {c.focus()};
-    }
-    else if (this.value.length === parseInt(this.attributes["maxlength"].value)) {
-        e.focus();
-    }
-});
-
-e.addEventListener('keydown', function(event) {
-    const key = event.key;
-    if (key === "Backspace") {
-        if (this.value.length < parseInt(this.attributes["maxlength"].value)) {d.focus()};
+    else {
+    requiredElement.innerHTML += key;
+    requiredElement.className = "boxDone";
+    requiredElement = elements[0]
     }
 });
