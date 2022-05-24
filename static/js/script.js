0232f418b63cc443d6a3ce97726fe7017fe9bde5
Patryk <pat.parkitny@gmail.com>
2022-05-24 22:10:32 +0200

    Add checking if user win or lose. Add block game after win or lose. Add showing word after end of the game

--- static/js/script.js
+++ static/js/script.js
@@ -16,12 +16,12 @@ document.addEventListener('keydown', function(event) { // listening for typing o
     const key = event.key;
     var letters = /^[A-Za-z]+$/; // only letters
     var i = 0; // set variable 'i' to zero to begin checking letters from begining
+    var win = 0; // set variable 'win' to zero to begin checking winning from begining
     if (key === "Backspace") { // check if user typre 'Backspace'
         if(document.getElementsByClassName('boxDone').length > 0){
             var delElement = Array.from(
                 document.getElementsByClassName('boxDone') // get last element with class 'boxDone'
             ).pop();
-            
             delElement.innerHTML = ""; // change value of div to empty
             delElement.className = "box"; // change name of class to 'box'
             requiredElement = elements[0] // again get first element with 'box' class
@@ -29,7 +29,9 @@ document.addEventListener('keydown', function(event) { // listening for typing o
             requiredElement.setAttribute('name', ''); 
         }
     }
-    else if(String.fromCharCode(event.keyCode).match(letters)) { // check if user type letter
+    else if(String.fromCharCode(event.keyCode).match(letters) && 
+            document.getElementsByClassName('lineWin').length < 1 && 
+            document.getElementsByClassName('box').length > 0) { // check if user type letter
         if(document.getElementsByClassName('boxDone').length < 5){ // check if there is only 5 divs with class "boxDone"
             requiredElement.innerHTML = key.toUpperCase();
             count++;
@@ -45,7 +47,6 @@ document.addEventListener('keydown', function(event) { // listening for typing o
             Array.from(elArray).forEach(element => { // Create string to check if user type word that exist
                 temp_string += element.innerHTML;
             })
-
             if (whole_all_words.includes(temp_string)) { // check if word exist in our db
                 show_word.innerHTML = ("This is a word :)");
                 show_word.style.color = '#121214';
@@ -53,33 +54,54 @@ document.addEventListener('keydown', function(event) { // listening for typing o
                     if (word_name.includes(element.innerHTML)) { // check if given word includes letter from box
                         if (element.innerHTML == word_name[i]){ // check if letter from box is on right opsition
                             element.className = "boxGreen";  // change class name to another with another css style
+                            win++;
+                            console.log(win)
+                            if (win == 5) {
+                                element.parentElement.className = "lineWin"; // win of the game!
+                                show_word.innerHTML = (word_name); // show msg to user
+                                show_word.style.color = 'Green';
+                                // set every boxex to done, beacuse of end of the game
+                                Array.from(document.getElementsByClassName('box')).forEach(element => {
+                                    element.className = "boxBlack";
+                                })
+                            }
                         }
                         else {
                             element.className = "boxYellow"; // change class name to another with another css style
+                            element.parentElement.className = "lineDone";
+                            if (document.getElementsByClassName('lineDone').length == 6){ // if user lose, game just ends
+                                show_word.innerHTML = (word_name); // show msg to user
+                                show_word.style.color = 'Red';
+                            }
                         }
                     }
                     else {
                         element.className = "boxBlack"; // change class name to another with another css style
+                        element.parentElement.className = "lineDone";
+                        if (document.getElementsByClassName('lineDone').length == 6){ // if user lose, game just ends
+                            show_word.innerHTML = (word_name); // show msg to user
+                            show_word.style.color = 'Red';
+                        }
                     }
                     i++; // increment variable 'i' to check all the letters and positions in word
                 });
             }
             else {
                 if (show_word.style.color == "red" ){ 
-                    show_word.innerHTML = ("This is not a word!"); // show msg to user
+                    show_word.innerHTML = ("Still not a word!"); // show msg to user
                     show_word.style.color = 'pink';
                 }
                 else {
-                show_word.innerHTML = ("This is not a word!"); // show msg to user
+                show_word.innerHTML = ("That's not a word!"); // show msg to user
                 show_word.style.color = 'red';
                 }
             }
         }
-        else {
+        else if (document.getElementsByClassName('boxDone').length < 5 && document.getElementsByClassName('boxDone').length > 1) { // check if user fill whole line or if game is ended
             show_word.innerHTML = ("Fill whole line"); // show msg to user
             show_word.style.color = 'white';
         }
-     }
+    }
 });
 
 
