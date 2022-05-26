28990e29d789324242cfe442d16c5f937fd8d28a
Patryk <pat.parkitny@gmail.com>
2022-05-26 22:50:22 +0200

    Add new .txt file with words that will be in the draw pool. It is done from A - K letters, L - Ż will be edit later

--- guess/views.py
+++ guess/views.py
@@ -9,19 +9,24 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 
 class MainView(View):
-    """ This class represent main site of whole app
+    """ 
+    This class represent main site of whole app.
+    There are words from .txt files and a randomized word for guessing on every page refresh.
     """
 
     def get(self, request):
-        with open('game_words.txt', 'r') as f:  # open file and get words
+        # open file and get 5 letters words that are acceptable in games
+        with open('acceptable_words.txt', 'r') as f:
             my_words = []  # create list
             for w in f:  # loop for taking every word and put it in list
-                my_words.append(w.upper())
+                my_words.append(w.upper())  # add word to list
 
+        # create long string with these words to easy check if user word is word or if it is acceptable
         all_words = ' '.join(my_words)
         random.shuffle(my_words)  # shuffle list to get random word
-        word = my_words[0]
+        word = my_words[0]  # take first random word
 
+        # send variables to site
         context = {'word': word, 'all_words': all_words}
         return render(request, 'main.html', context)
 
