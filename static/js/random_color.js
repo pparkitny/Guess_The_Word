2bd9f0a2ad9ecfba194b766f07d5904cb845256b
Patryk <pat.parkitny@gmail.com>
2022-05-27 00:13:00 +0200

    Add changing the colors of the main text after hover mouse over it

---
+++ static/js/random_color.js
@@ -0,0 +1,22 @@
+function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+  } 
+  
+  
+  document.addEventListener('mouseover', event => {
+      if (document.querySelector('a') == event.target) {
+        console.log("KURDE")  
+        event.target.style.color = randomColor();
+      }
+  });
+
+  document.addEventListener('mouseout', event => {
+    if (document.querySelector('a') == event.target) {
+      console.log("KURDE")  
+      event.target.style.color = randomColor();
+    }
+});
